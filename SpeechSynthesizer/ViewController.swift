1593cb3d647add293d7446e35ccbfc8a5a56d0f3
ChingWen <fsvnxwx@gmail.com>
2020-08-16 01:04:02 +0800

    Initial Commit

---
+++ SpeechSynthesizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeechSynthesizer
+//
+//  Created by eric on 2020/8/16.
+//  Copyright © 2020 eric. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
